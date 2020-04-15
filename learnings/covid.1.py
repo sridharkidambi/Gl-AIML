058274fd61017bf2f62630461befd1d17445c268
sridhar.kidambi@wipro.com <sridhar.kidambi@wipro.com>
2020-04-15 10:04:07 +0530

    code changes

--- learnings/covid.1.py
+++ learnings/covid.1.py
@@ -8,7 +8,7 @@ from datetime import date
 import datetime
 sns.set(color_codes=True)
 
-STATE="Tamil Nadu"
+STATE="Maharashtra"
 
 df_base_date = pd.read_csv("Stateanddistwisecount2.csv");
 bappend:bool=False;
@@ -62,7 +62,7 @@ if(bappend):
     sns.pointplot(x=dfObj[dfObj["state"]==STATE]["district"],y=dfObj[dfObj["state"]==STATE]["confirmed_count"],hue=dfObj[dfObj["state"]==STATE]["date"]);
     plt.savefig(STATE + "_district_wise_daily_count"+ str(date.today())+".png")
 else:
-    # sns.scatterplot(x=df_base_date[df_base_date["state"]==STATE]["district"],y=df_base_date[df_base_date["state"]==STATE]["confirmed_count"],hue=df_base_date[df_base_date["state"]==STATE]["date"]);
+    sns.barplot(x=df_base_date[df_base_date["state"]==STATE]["district"],y=df_base_date[df_base_date["state"]==STATE]["confirmed_count"],hue=df_base_date[df_base_date["state"]==STATE]["date"]);
     # sns.pointplot(x=df_base_date[df_base_date["state"]==STATE]["district"],y=df_base_date[df_base_date["state"]==STATE]["confirmed_count"],hue=df_base_date[df_base_date["state"]==STATE]["date"]);
     plt.savefig(STATE + "_district_wise_daily_count"+ str(date.today())+".png")
 
@@ -73,11 +73,11 @@ ax = fig.add_subplot(111)
 plt.setp(ax.get_xticklabels(), rotation=30, horizontalalignment='right')
 if(bappend):
     # sns.countplot(x="incr_percentage", hue="date", data=dfObj[dfObj["state"]==STATE]["district"])
-    # sns.scatterplot(x=dfObj[dfObj["state"]==STATE]["district"],y=dfObj[dfObj["state"]==STATE]["incr_percentage"],hue=dfObj[dfObj["state"]==STATE]["date"]);
+    sns.scatterplot(x=dfObj[dfObj["state"]==STATE]["district"],y=dfObj[dfObj["state"]==STATE]["incr_percentage"],hue=dfObj[dfObj["state"]==STATE]["date"]);
     plt.savefig(STATE + "_district_wise_daily_percent_count"+ str(date.today())+".png")
 else:
     df_base_date.reset_index(inplace=True);
-    sns.barplot(x=df_base_date[df_base_date["state"]==STATE]["district"],y=df_base_date[df_base_date["state"]==STATE]["incr_percentage"], hue=df_base_date[df_base_date["state"]==STATE]["date"])
+    # sns.barplot(x=df_base_date[df_base_date["state"]==STATE]["district"],y=df_base_date[df_base_date["state"]==STATE]["incr_percentage"], hue=df_base_date[df_base_date["state"]==STATE]["date"])
     # sns.barplot(x="incr_percentage", hue="date", data=df_base_date[df_base_date["state"]==STATE]["district"])
     # sns.scatterplot(x=df_base_date[df_base_date["state"]==STATE]["district"],y=df_base_date[df_base_date["state"]==STATE]["incr_percentage"],hue=df_base_date[df_base_date["state"]==STATE]["date"]);
     plt.savefig(STATE + "_district_wise_daily_percent_count"+ str(date.today())+".png")
